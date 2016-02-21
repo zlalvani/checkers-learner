e7b7af9160bff65850c932cfb26127f41bcb8caf
zlalvani <zlalvani@gmail.com>
2016-02-21 18:00:18 -0500

    started implementation of move checker

---
+++ game.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+class Board:
+	'A class to represent board states, built around 2D numpy.array'
+	def __init__(self):
+
+
+def verifyMove():
+	pass
+
+
+def getMoveList(state):
+	pass
---
+++ provision.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+sudo apt-get install -y git
+sudo apt-get install -y build-essential
+
+sudo pip install numpy
