dfe2a3fe5cadc05515dc709029ee423e15c69485
Zaran <zlalvani@gmail.com>
2016-03-22 17:36:39 -0400

    started learner

---
+++ learner.py
@@ -0,0 +1,14 @@
+import numpy as np
+from globalconsts import \
+	EMPTY, RED, BLACK, BKING, RKING, \
+	FORWARD_LEFT, FORWARD_RIGHT, BACKWARD_LEFT, BACKWARD_RIGHT
+
+class Learner(object):
+	'''
+	A class that instantiates the feature space for an individual AI, 
+	chooses moves, and performs learning
+	'''
+	def __init__(self, contents = []):
+		pass
+
+#http://scikit-learn.org/stable/modules/neighbors.html#classification
